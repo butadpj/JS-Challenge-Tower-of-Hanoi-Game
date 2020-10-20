0515751a19b9ab5fd38ff0841144f307458da1d5
butadpj <butadpj@gmail.com>
2020-10-20 17:06:14 +0800

    Tower of Hanoi V2

--- script.js
+++ script.js
@@ -1,90 +1,80 @@
 let t1 = document.querySelector('#t1')
 let t2 = document.querySelector('#t2')
 let t3 = document.querySelector('#t3')
-
-let activeDisk = []
-let moves = 0
-
-let t1d = document.querySelector('.t1d')
-let t2d = document.querySelector('.t2d')
-let t3d = document.querySelector('.t3d')
-
-let dText = document.querySelector('.destination')
-let destination = 3
-let moveText = document.querySelector('.moves')
-
-dText.innerHTML = `MOVE ALL THE DISC TO TOWER: ${3}`
+let activeDisc = []
+let totalMoves = 0
+checkDestination(3)
 
 document.addEventListener('click', function(e){
     if (e.target.classList.contains('disc')) {
         let currentDisc = e.target
-        let previousDisc = currentDisc.previousElementSibling
-        if (previousDisc == null) {
-            e.target.classList.add('active')
-            activeDisk.unshift(e.target)
-        } else {
+        let beforeDisc = e.target.previousElementSibling
+
+        if (beforeDisc == null) {
+            activeDisc.unshift(currentDisc)
+            activeDisc[0].classList.add('active') 
+        }        
+    }
+})
+
+document.addEventListener('click', function(e) {
+    if (activeDisc.length > 0) {
+        if (e.target === activeDisc[0]) {
             return
+        } else {
+            activeDisc[0].classList.remove('active')
+            let moves = document.querySelector('.moves')
+            totalMoves = totalMoves + 1
+            moves.innerHTML = totalMoves
+            activeDisc = []
         }
     }
 })
 
-document.addEventListener('click', function(e){
-    if (e.target == activeDisk[0]) {
-        return
-    }
-    if (activeDisk.length > 0) {
-        activeDisk[0].classList.remove('active')
-        activeDisk.pop()
-        moves = moves + 1
-        moveText.innerHTML = moves
+t1.addEventListener('click', function(e){
+    if (activeDisc.length > 0) {
+        updateDisc('t1')
     }
 })
 
-t1.addEventListener('click', function(){
-    if (activeDisk.length > 0) {
-        checkDisk(t1d)
+t2.addEventListener('click', function(e){
+    if (activeDisc.length > 0) {
+        updateDisc('t2')
+        
     }
 })
 
-t2.addEventListener('click', function(){
-    if (activeDisk.length > 0) {
-        checkDisk(t2d)
+t3.addEventListener('click', function(e){
+    if (activeDisc.length > 0) {
+        updateDisc('t3')
+        //Destination -- Move the function and change the selected disc array
+        checkDiscs(document.querySelector('.t3d'))
     }
 })
 
-t3.addEventListener('click', function(){
-    if (activeDisk.length > 0) {
-        checkDisk(t3d)
-        checkGame(t3d)
-    }
-})
 
-function checkGame(tower) {
-    console.log(tower.children.length)
-    if (tower.children.length == 3) {
-        setTimeout(function(){alert(`CONGRATS! You completed the game with ${moves} moves`)}, 300) 
-        setTimeout(function(){location.reload()}, 300) 
-    }
-}
-
-function checkDisk(diskTower) {
-    let currentDiskSize = Number(activeDisk[0].id.slice(-1))
-    console.log(diskTower.children[0])
-    if (diskTower.children[0] != undefined) {
-        let afterDiskSize = diskTower.children[0].id.slice(-1)
-
-        if (currentDiskSize > afterDiskSize) {
+function updateDisc(tower) {
+    let towerDisc = document.querySelector(`.${tower}d`)
+    let beforeDisc = towerDisc.children[0]
+    let activeWeight = activeDisc[0].id.slice(-1)
+    if (beforeDisc != undefined) {
+        let beforeWeight = beforeDisc.id.slice(-1)
+        if (activeWeight > beforeWeight) {
             return
-        } else {
-            diskTower.prepend(activeDisk[0])
         }
-    } else {
-        diskTower.prepend(activeDisk[0])
     }
-    
-
+    towerDisc.prepend(activeDisc[0])
     
 }
 
+function checkDiscs(array) {
+    if (array.children.length == 3) {
+        setTimeout(function() {alert(`Good Job! You complete the game with ${totalMoves} moves`)}, 100)
+        setTimeout(function() {location.reload()}, 300)
+    }
+}
 
-
+function checkDestination(destination) {
+    let towerDestinaton = document.querySelector('.destination')
+    towerDestinaton.innerHTML = `MOVE ALL THE DISC TO TOWER: ${destination}`
+}
